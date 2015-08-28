245f563aebac8ba2a06f06cdf6c266e965447751
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2015-08-28 20:55:12 +0530

    UPDATE: formatting with phpcbf
    SUMMARY:
        * files are updated with phpcbf
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- database/migrations/2015_08_28_135335_create_subjects_table.php
+++ database/migrations/2015_08_28_135335_create_subjects_table.php
@@ -12,7 +12,7 @@ class CreateSubjectsTable extends Migration
      */
     public function up()
     {
-        Schema::create('subjects', function(Blueprint $table){
+        Schema::create('subjects', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->char('code', 3);
