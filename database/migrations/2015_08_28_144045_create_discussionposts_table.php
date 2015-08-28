a29973447e73f9a98dd7a07b2af519348579ee26
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2015-08-28 20:29:22 +0530

    UPDATE: adding migrations for discussions,discussionposts
    SUMMARY:
        * adding migration,model for Discussion
        * adding migration,model for DiscussionPost
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

---
+++ database/migrations/2015_08_28_144045_create_discussionposts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiscussionpostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discussion_posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->longText('post');
+            $table->integer('discussion_id')->unsigned();
+            $table->foreign('discussion_id')->references('id')->on('discussions')
+                ->onDelete('cascade');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('discussion_posts');
+    }
+}
