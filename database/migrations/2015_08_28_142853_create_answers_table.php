5e2568e321bc0db406625b7561c67ca62d9f3ed5
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2015-08-28 20:48:58 +0530

    UPDATE: updating migrations
    SUMMARY:
        * updating answers migration file
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- database/migrations/2015_08_28_142853_create_answers_table.php
+++ database/migrations/2015_08_28_142853_create_answers_table.php
@@ -14,6 +14,13 @@ class CreateAnswersTable extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('type');
+            $table->blob('resource');
+            $table->longText('textual');
+            $table->boolean('correct');
+            $table->integer('question_id')->unsigned();
+            $table->foreign('question_id')->references('id')->on('questions')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
