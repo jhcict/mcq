45ee2a74a09f7e68bb54e50615a6546b2635d0c9
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2015-08-29 20:55:21 +0530

    UPDATE: updating sidebar
    SUMMARY:
        * updating sidebar with proper urls
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- resources/views/admin/layouts/widgets/sidebar.blade.php
+++ resources/views/admin/layouts/widgets/sidebar.blade.php
@@ -1,13 +1,8 @@
 <ul class="nav nav-sidebar">
     <li><a href="{{action('Admin\HomeController@getIndex')}}">Dashboard</a></li>
-    <li><a href="{{action('Admin\TutorialController@index')}}">Tutorials</a></li>
-    <li><a href="{{action('Admin\AssessmentController@index')}}">Assessments</a></li>
-    <li><a href="{{action('Admin\ExamController@index')}}">Exams</a></li>
-    <li><a href="{{action('Admin\PresentationController@index')}}">Presentations</a></li>
-    <li class="nav-divider"></li>
     <li><a href="{{action('Admin\SubjectController@index')}}">Subjects</a></li>
-    <li><a href="{{action('Admin\StudentController@index')}}">Students</a></li>
-    <li><a href="{{action('Admin\TeacherController@index')}}">Teachers</a></li>
+    <li><a href="{{action('Admin\PaperController@index')}}">Papers</a></li>
+    <li><a href="{{action('Admin\CategoryController@index')}}">Category</a></li>
     <li><a href="{{action('Admin\UserController@index')}}">Users</a></li>
-    <li><a href="{{action('Admin\SettingsController@getIndex')}}">Settings</a></li>
+    <li><a href="{{action('Admin\RoleController@index')}}">Roles</a></li>
 </ul>
