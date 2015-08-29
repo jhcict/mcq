967f6009d53c42b49d75921bd13e306697e5fb55
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2015-08-29 19:40:55 +0530

    UPDATE: updating layout
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Edlara v2</title>
+    <title>learnMCQ</title>
 
     <link rel="stylesheet" href="/library/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/library/bootstrap/css/bootstrap-theme.min.css">
@@ -29,7 +29,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Edlara v2</a>
+                <a class="navbar-brand" href="#">learnMCQ</a>
             </div>
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -44,7 +44,9 @@
                     @else
                         <li><a href="/admin">Admin</a></li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->first_name }} {{Auth::user()->last_name}} <span class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->first_name }} {{Auth::user()->last_name}} <span class="caret"></span>
+                            </a>
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="/auth/logout">Logout</a></li>
                             </ul>
