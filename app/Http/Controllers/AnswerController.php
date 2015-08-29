8c2e8f5cf605cfdd0fc48b618b1c86c06c6f0935
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2015-08-29 20:11:03 +0530

    UPDATE: updating routes
    SUMMARY:
        * phpcbf applied on controllers
        * route file updated with Docblock
        * route file updated with proper routes
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -32,7 +32,7 @@ class AnswerController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Request  $request
+     * @param  Request $request
      * @return Response
      */
     public function store(Request $request)
@@ -43,7 +43,7 @@ class AnswerController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return Response
      */
     public function show($id)
@@ -54,7 +54,7 @@ class AnswerController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return Response
      */
     public function edit($id)
@@ -65,8 +65,8 @@ class AnswerController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  Request  $request
-     * @param  int  $id
+     * @param  Request $request
+     * @param  int     $id
      * @return Response
      */
     public function update(Request $request, $id)
@@ -77,7 +77,7 @@ class AnswerController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return Response
      */
     public function destroy($id)
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,36 @@
 <?php
+/**
+ * Routes File
+ *
+ * PHP Version 5
+ *
+ * @category Routing
+ * @package  LearnMCQ
+ * @author   Gnanakeethan Balasubramaniam <gnana@keethan.me>
+ * @license  MIT https://license.mit.edu
+ * @link     https://mcq.learntechnologies.org/
+ */
+
+Route::controller('/', 'HomeController');
+Route::resource('paper', 'PaperController');
+Route::resource('questions', 'QuestionController');
+Route::resource('answer', 'AnswerController');
+Route::resource('discussion', 'DiscussionControoler');
+Route::resource('discussionpost', 'DiscussionPostController');
 
 
-Route::get('/',function(){
-return view('index');
-});
+Route::group(
+    [
+    'prefix'=> 'admin',
+    'namespace'=>'admin',
+    'middleware'=>'admin'],
+    function () {
+        Route::resource('subjects', 'SubjectController');
+        Route::resource('papers', 'PaperController');
+        Route::resource('questions', 'QuestionController');
+        Route::resource('users', 'UserController');
+        Route::resource('roles', 'RoleController');
+        Route::resource('category', 'CategoryController');
+        Route::resource('answer', 'AnswerController');
+    }
+);
