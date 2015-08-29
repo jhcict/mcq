f7cc19fe194011f15e33fdca543d97d0339c68d0
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2015-08-29 08:12:48 +0530

    UPDATE: adding admin subject controller
    SUMMARY:
        * adding Admin/SubjectController
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

---
+++ app/Http/Controllers/Admin/SubjectController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+/**
+ * Class: SubjectController
+ *
+ * @see
+ */
+class SubjectController extends Controller
+{
+
+    /**
+     * Display a listing of Subjects available as of now
+     *
+     * @param Subject $subject Model Class
+     *
+     * @return Response
+     */
+    public function index(Subject $subject)
+    {
+        $subjects = $subject->all();
+
+        return view('admin.subject.index', compact($subjects));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request $request
+     * @param  int     $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
