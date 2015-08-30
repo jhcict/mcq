46c5a051eb21344d7ce4296d2afbf5c9896d88ef
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2015-08-30 10:25:53 +0530

    UPDATE: fixing registration
    SUMMARY:
        * added unit tests for registration
        * modified registration view
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,15 +22,9 @@
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                         <div class="form-group">
-                            <label class="col-md-4 control-label">First Name</label>
+                            <label class="col-md-4 control-label">Name</label>
                             <div class="col-md-6">
-                                <input type="text" class="form-control" name="first_name" value="{{ old('first_name') }}">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">Last Name</label>
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="last_name" value="{{ old('last_name') }}">
+                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                             </div>
                         </div>
 
