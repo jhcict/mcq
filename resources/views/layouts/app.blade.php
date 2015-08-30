9fd15b8983f5a119cdac12c0a13a391127106d49
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2015-08-30 06:57:33 +0530

    FIX: fixing a layout issue
    SUMMARY:
        * #1 fix
        * fixes issue in displaying user's proper name
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
                         <li><a href="/admin">Admin</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->first_name }} {{Auth::user()->last_name}} <span class="caret"></span>
+                                {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="/auth/logout">Logout</a></li>
