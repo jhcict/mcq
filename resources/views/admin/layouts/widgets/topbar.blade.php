dc3903de1ee162b5796673370e019b3a4feaa3f1
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2015-09-03 20:14:23 +0530

    FIX: fixing travis error
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- resources/views/admin/layouts/widgets/topbar.blade.php
+++ resources/views/admin/layouts/widgets/topbar.blade.php
@@ -21,7 +21,7 @@
                 @if (Auth::guest())
                     <li><a href="/auth/login">Login</a></li>
                     <li><a href="/auth/register">Register</a></li>
-                @else
+                @elseif(Auth::check())
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{Auth::user()->name}} <span class="caret"></span>
 </a>
