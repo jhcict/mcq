3ed171fa4d5699b1462bb3de1f3f50fc2e452818
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2016-04-02 07:31:19 +0530

    UPDATE
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => [ 'address' => null, 'name' => null ],
+    'from' => [ 'address' => 'info@mcq.learntechnologies.org', 'name' => 'Learn Tech'],
 
     /*
     |--------------------------------------------------------------------------
